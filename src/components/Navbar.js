1d5ac0886857989b6de22c3d94c186eae1cb36ce
Calebbii <biicaleb316@gmail.com>
2024-11-11 19:59:00 +0300

    Nested routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/navbar.css'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>
+                <Link to="/home">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
